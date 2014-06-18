62fb24354505107d7ac9d315be96130611d9a6ab
jlchereau <jlchereau@msn.com>
2014-06-18 08:32:47 +0100

    npm install

--- setup.cmd
+++ setup.cmd
@@ -1,9 +1,8 @@
 cd /d %~dp0
 call "%ProgramFiles%\nodejs\nodevars.bat"
-npm install -g grunt-cli
 npm install -g ripple-emulator
-REM npm install -g mocha
-REM npm install -g karma-cli
-REM npm install -g istanbul
-
-
+npm install -g grunt-cli
+npm install -g mocha
+npm install -g karma-cli
+npm install -g istanbul
+npm install
