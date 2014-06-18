d8ff9ce30e84685dccc3b1ced8dcd40fc4b46f1d
jlchereau <jlchereau@msn.com>
2014-06-18 08:29:23 +0100

    Also update app.rapi.js

--- update.cmd
+++ update.cmd
@@ -1,4 +1,3 @@
 cd /d %~dp0
-xcopy %USERPROFILE%\CREATI~1\Kidoju\Kidoju.Widgets\dist\*.* %USERPROFILE%\CREATI~1\Kidoju\Kidoju.Mobile\www\*.* /S /Y
-xcopy %USERPROFILE%\CREATI~1\Kidoju\Kidoju.Server\client\*.* %USERPROFILE%\CREATI~1\Kidoju\Kidoju.Mobile\www\*.* /S /Y
-del %USERPROFILE%\CREATI~1\Kidoju\Kidoju.Mobile\www\.jshintrc
+xcopy %USERPROFILE%\CREATI~1\Kidoju\Kidoju.Widgets\dist\*.* %USERPROFILE%\CREATI~1\Kidoju\Kidoju.Mobile\www\*.* /S /C /Y
+copy %USERPROFILE%\CREATI~1\Kidoju\Kidoju.Server\client\js\app.rapi.js %USERPROFILE%\CREATI~1\Kidoju\Kidoju.Mobile\www\js\app.rapi.js /Y
