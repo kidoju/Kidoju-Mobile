38a3b831f73e0e3336f5f06275f2df9411c9dc05
jlchereau <jlchereau@msn.com>
2014-07-05 10:16:05 +0100

    copy app.cache.js

--- update.cmd
+++ update.cmd
@@ -1,3 +1,4 @@
 cd /d %~dp0
 xcopy ..\Kidoju.Widgets\dist\*.* www\*.* /S /C /Y
 copy ..\Kidoju.Server\client\js\app.rapi.js www\js\app.rapi.js /Y
+copy ..\Kidoju.Server\client\js\app.cache.js www\js\app.cache.js /Y
