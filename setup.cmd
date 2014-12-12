29e3415e9482058bd5de025065253edf30189fdb
jlchereau <jlchereau@msn.com>
2014-12-12 09:04:28 +0000

    Updated setup

--- setup.cmd
+++ setup.cmd
@@ -1,8 +1,18 @@
 cd /d %~dp0
 call "%ProgramFiles%\nodejs\nodevars.bat"
-npm install -g ripple-emulator
+REM update npm, which is a requirement for some modules
+npm install -g npm
+REM grunt command in terminal mode
 npm install -g grunt-cli
-npm install -g mocha
-npm install -g karma-cli
+REM code coverage with istanbul
 npm install -g istanbul
+REM karma command in terminal mode
+npm install -g karma-cli
+REM mocha command in terminal mode
+npm install -g mocha
+REM phonegap command in terminal mode
+npm install -g phonegap
+REM ripple emulator
+npm install -g ripple-emulator
+REM install all local modules in package.json
 npm install
