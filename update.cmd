4a1735c3eacda25559fb070f4dd87b369191d894
jlchereau <jlchereau@msn.com>
2014-12-12 09:07:31 +0000

    Updated update

--- update.cmd
+++ update.cmd
@@ -1,4 +1,16 @@
 cd /d %~dp0
+
+REM Copy Kidoju webfonts
+REM xcopy ..\Kidoju.WebFonts\dist\fonts\*.* www\styles\fonts\*.* /S /C /Y
+
+REM Copy Memba widgets
+xcopy ..\..\Memba\Memba.Widgets\dist\*.* www\*.* /S /C /Y
+
+REM Copy Kidoju widgets
 xcopy ..\Kidoju.Widgets\dist\*.* www\*.* /S /C /Y
+
 copy ..\Kidoju.Server\client\js\app.rapi.js www\js\app.rapi.js /Y
 copy ..\Kidoju.Server\client\js\app.cache.js www\js\app.cache.js /Y
+copy ..\Kidoju.Server\client\js\app.models.js www\js\app.models.js /Y
+
+copy ..\Kidoju.Server\test\client\app.models.mock.js www\js\app.models.mock.js /Y
