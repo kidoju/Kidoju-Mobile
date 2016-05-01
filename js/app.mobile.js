8688c37556f8ac96507d162d613f7c11f0033385
jlchereau <jlchereau@msn.com>
2016-05-01 08:43:13 +0100

    Added app.mobile.js

--- js/app.mobile.js
+++ js/app.mobile.js
@@ -6,15 +6,27 @@
 /* jshint browser: true, jquery: true */
 /* globals define: false, require: false */
 
+if (typeof(require) === 'function') {
+
+    // Load kendo CSS
+    require('../styles/vendor/kendo/web/kendo.common.less');
+    // require('../styles/vendor/kendo/web/kendo.rtl.less');
+    require('../styles/vendor/kendo/web/kendo.default.less');
+    require('../styles/vendor/kendo/mobile/kendo.mobile.all.less');
+
+    // Load app CSS
+    require('../styles/app.mobile.less');
+}
 
 (function (f, define) {
     'use strict';
     define([
-        './window.assert',
-        './window.logger',
-        './app.logger',
-        './app.i18n',
-        './app.rapi'
+        './vendor/kendo/kendo.all'
+        // './window.assert',
+        // './window.logger',
+        // './app.logger',
+        // './app.i18n',
+        // './app.rapi'
     ], f);
 })(function () {
 
@@ -24,8 +36,46 @@
 
         var kendo = window.kendo;
         var app = window.app || {};
-        
-        
+
+        // Application Constructor
+        app.initialize = function() {
+            this.bindEvents();
+        };
+
+        /**
+         * Bind Event Listeners
+         *
+         * Bind any events that are required on startup. Common events are:
+         * 'load', 'deviceready', 'offline', and 'online'.
+         */
+        app.bindEvents = function() {
+            document.addEventListener('deviceready', this.onDeviceReady, false);
+        };
+
+        /**
+         * deviceready Event Handler
+         *
+         * The scope of 'this' is the event. In order to call the 'receivedEvent'
+         * function, we must explicitly call 'app.receivedEvent(...);'
+         */
+        app.onDeviceReady = function() {
+            app.receivedEvent('deviceready');
+        };
+
+        /**
+         * Update DOM on a Received Event
+         * @param id
+         */
+        app.receivedEvent = function(id) {
+            var parentElement = document.getElementById(id);
+            var listeningElement = parentElement.querySelector('.listening');
+            var receivedElement = parentElement.querySelector('.received');
+
+            listeningElement.setAttribute('style', 'display:none;');
+            receivedElement.setAttribute('style', 'display:block;');
+
+            console.log('Received Event: ' + id);
+        };
 
     }(window.jQuery));
 
