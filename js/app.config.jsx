f4f0b66391461c2935b20caf241ac2190b187b66
jlchereau <jlchereau@msn.com>
2016-05-18 16:43:33 +0100

    Copied from Kidoju-WebApp

---
+++ js/app.config.jsx
@@ -0,0 +1,164 @@
+/**
+ * Copyright (c) 2013-2016 Memba Sarl. All rights reserved.
+ * Sources at https://github.com/Memba
+ */
+
+/* jshint browser: true */
+/* globals define: false, __VERSION__: false */
+
+(function (f, define) {
+    'use strict';
+    define([
+        './window.assert',
+        './window.logger',
+        './app.logger'
+    ], f);
+})(function () {
+
+    'use strict';
+
+    /**
+     * Note: This file is built with webpack using ./web_modules/jsx-loader.
+     * Values are read from any of the JSON config files in ./webapp/config
+     * depending on NODE_ENV: development, test or production (by default).
+     */
+
+    (function () {
+
+        var app = window.app = window.app || {};
+        // var assert = window.assert;
+        var logger = new window.Logger('app.config');
+
+        /**
+         * Join url bits, adding slashes where required
+         * TODO: This could be improved to account for . and ..
+         */
+        var url = {
+            join: function () {
+                // Actually we first join with slashes, then we replace double or triple slashes, except when preceded by colons like in http://
+                return Array.prototype.slice.call(arguments).join('/').replace(/([^:])[\/]{2,}/g, '$1/');
+            }
+        };
+
+        /**
+         * application DEBUG mode
+         * @type {boolean}
+         */
+        app.DEBUG = '<%- debug %>'.toLowerCase() === 'true';
+
+        /**
+         * application version
+         * Note: this is the only way to do it because version does not exist in configuration files loaded by ./web_modules/jsx_loader
+         */
+        app.version = __VERSION__;
+
+        /**
+         * application locales
+         */
+        app.locales = JSON.parse('<%- JSON.stringify(locales) %>');
+
+        /**
+         * Logger
+         */
+        window.Logger.prototype.level = parseInt('<%- level %>', 10) || 0;
+        app.logger.level = parseInt('<%- level %>', 10) || 0;
+        app.logger.token = '<%- logentries.browser.token %>';
+
+        /**
+         * Facebook
+         * @type {{clientId: string}}
+         */
+        app.facebook = { clientID: '<%- facebook.clientID %>' };
+
+        /**
+         * Twitter
+         * @type {{account: string}}
+         */
+        app.twitter = { account: '<%- twitter.account %>' };
+
+        /**
+         * Convert nodejs printf like formatting strings into Kendo UI formatting strings
+         * where %s placeholders are replaced with {i} placeholders
+         * @see https://nodejs.org/api/util.html#util_util_format_format
+         * @see http://docs.telerik.com/kendo-ui/api/javascript/kendo#methods-format
+         * @param value
+         * @returns {*}
+         */
+        function convertFormat (value) {
+            var i = 0;
+            var ret = value;
+            var rx = /%[sdj]/;
+            while (typeof ret === 'string' && rx.test(ret)) {
+                ret = ret.replace(rx, '{' + i + '}');
+                i++;
+            }
+            return ret;
+        }
+
+        /**
+         * Assets for Asset Manager
+         * @type {{audio: {collections, extensions, schemes, transport}, image: {collections, extensions, schemes, transport}, video: {collections, extensions, schemes, transport}}}
+         */
+        app.assets = {
+
+            audio: {
+                collections: JSON.parse('<%- JSON.stringify(assets.audio.collections) %>'),
+                extensions: JSON.parse('<%- JSON.stringify(assets.audio.extensions) %>'),
+                schemes: JSON.parse('<%- JSON.stringify(assets.audio.schemes) %>')
+                // transport: JSON.parse('<%- JSON.stringify(assets.audio.transport) %>')
+            },
+
+            image: {
+                collections: JSON.parse('<%- JSON.stringify(assets.image.collections) %>'),
+                extensions: JSON.parse('<%- JSON.stringify(assets.image.extensions) %>'),
+                schemes: JSON.parse('<%- JSON.stringify(assets.image.schemes) %>')
+                // transport: JSON.parse('<%- JSON.stringify(assets.image.transport) %>')
+            },
+
+            video: {
+                collections: JSON.parse('<%- JSON.stringify(assets.video.collections) %>'),
+                extensions: JSON.parse('<%- JSON.stringify(assets.video.extensions) %>'),
+                schemes: JSON.parse('<%- JSON.stringify(assets.video.schemes) %>')
+                // transport: JSON.parse('<%- JSON.stringify(assets.video.transport) %>')
+            }
+
+        };
+
+        /**
+         * Application URIs
+         * See /wepapp/middleware/locals.js
+         * ATTENTION, contrary to server-side uris client-side uris are all concatenated with root expect for rapi
+         */
+        app.uris = {
+            rapi: {
+                root: '<%- uris.rapi.root %>'
+            },
+            cdn: {
+                icons: url.join('<%- uris.cdn.root %>', convertFormat('<%- uris.cdn.icons %>'))
+            },
+            webapp: {
+                editor: url.join('<%- uris.webapp.root %>', convertFormat('<%- uris.webapp.editor %>')),
+                // error
+                finder: url.join('<%- uris.webapp.root %>', convertFormat('<%- uris.webapp.finder %>')),
+                home: url.join('<%- uris.webapp.root %>', convertFormat('<%- uris.webapp.home %>')),
+                locale: '<%- uris.webapp.root %>' + convertFormat('<%- uris.webapp.locale %>'), // redirection when changing locale
+                ping: url.join('<%- uris.webapp.root %>', convertFormat('<%- uris.webapp.ping %>')),
+                player: url.join('<%- uris.webapp.root %>', convertFormat('<%- uris.webapp.player %>')),
+                public: url.join('<%- uris.webapp.root %>', convertFormat('<%- uris.webapp.public %>')),
+                rss: url.join('<%- uris.webapp.root %>', convertFormat('<%- uris.webapp.rss %>')),
+                sitemap: url.join('<%- uris.webapp.root %>', convertFormat('<%- uris.webapp.sitemap %>')),
+                summary: url.join('<%- uris.webapp.root %>', convertFormat('<%- uris.webapp.summary %>')),
+                user: url.join('<%- uris.webapp.root %>', convertFormat('<%- uris.webapp.user %>')),
+                workerlib: url.join('<%- uris.webpack.root %>', convertFormat('<%- uris.webapp.workerlib %>'))
+            }
+        };
+
+        logger.info({
+            message: 'app configured'
+        });
+
+    }());
+
+    return window.app;
+
+}, typeof define === 'function' && define.amd ? define : function (_, f) { 'use strict'; f(); });
---
+++ js/app.i18n.js
@@ -0,0 +1,123 @@
+/**
+ * Copyright (c) 2013-2016 Memba Sarl. All rights reserved.
+ * Sources at https://github.com/Memba
+ */
+
+/* jshint browser: true, jquery: true */
+/* globals define: false, require: false */
+
+(function (f, define) {
+    'use strict';
+    define([
+        './window.assert',
+        './window.logger',
+        './app.logger'
+    ], f);
+})(function () {
+
+    'use strict';
+
+    (function ($, undefined) {
+
+        var app = window.app;
+        var assert = window.assert;
+        var logger = new window.Logger('app.i18n');
+        var cultures = app.cultures = app.cultures || {};
+        var LOADED = 'i18n.loaded';
+        var STRING = 'string';
+        var ARRAY = 'array';
+
+        /**
+         * localization functions
+         */
+        var i18n = app.i18n = {
+
+            /**
+             * Load culture file for locale
+             * @param locale
+             * @param callback
+             */
+            load: function (locale) {
+
+                assert.type(ARRAY, app.locales, '`app.locales` is expected to be an array');
+                assert.enum(app.locales, locale, '`locale` is expected to be on of `app.locales`');
+
+                var dfd = $.Deferred();
+
+                // Setter
+                function setLocale() {
+                    i18n.culture = cultures[locale];
+                    dfd.resolve();
+                }
+
+                if (cultures[locale]) {
+                    // locale already loaded
+                    setLocale();
+                } else {
+                    // locale needs to be loaded (see https://github.com/webpack/webpack/issues/923)
+                    var loader = require('bundle?name=[name]!./cultures/app.culture.' + locale + '.js');
+                    loader(setLocale);
+                }
+
+                return dfd.promise();
+            },
+
+            /**
+             * Get/set locale
+             * Value set by the server on the html element of the page base on the url
+             * @param locale
+             * @returns {string|string}
+             */
+            locale: function (locale) {
+                if (typeof locale === STRING) {
+                    assert.type(ARRAY, app.locales, '`app.locales` is expected to be an array');
+                    assert.enum(app.locales, locale, '`locale` is expected to be on of `app.locales`');
+                    var href = app.uris.webapp.locale.replace('{0}', locale);
+                    if (window.top === window.self) {
+                        window.location.assign(href);
+                    } else {
+                        // This is an embedded player
+                        window.top.location.assign(href);
+                    }
+                } else if (locale === undefined) {
+                    return document.getElementsByTagName('html')[0].getAttribute('lang') || 'en';
+                } else {
+                    throw new TypeError('Bad locale: ' + locale);
+                }
+            }
+
+        };
+
+        /**
+         * Load page locale (read from html tag)
+         */
+        var locale = i18n.locale();
+        i18n.load(locale)
+            .then(function () {
+                $(document).ready(function () {
+
+                    // Log readiness
+                    logger.info({
+                        message: locale + ' locale loaded',
+                        method: '$(document).ready'
+                    });
+
+                    // trigger event for client localization of page
+                    $(document).trigger(LOADED);
+                });
+            });
+
+        /**
+         * Wait until locale is loaded to localize and hide preload
+         * @see http://blogs.telerik.com/kendoui/posts/11-10-06/foujui_flash_of_uninitialized_javascript_ui
+         */
+        $(document)
+            .on(LOADED, function () {
+                $('body>div.k-loading-image').fadeOut();
+            });
+
+    }(window.jQuery));
+
+    return window.app;
+
+}, typeof define === 'function' && define.amd ? define : function (_, f) { 'use strict'; f(); });
