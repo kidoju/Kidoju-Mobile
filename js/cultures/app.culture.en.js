ec3efff8f6796a885f51045e1b3c5927d1f04a00
jlchereau <jlchereau@msn.com>
2016-05-18 18:09:38 +0100

    Added mobile culture files

--- js/cultures/app.culture.en.js
+++ js/cultures/app.culture.en.js
@@ -1,3 +1,46 @@
 /**
- * Created by jlche on 23/03/2016.
+ * Copyright (c) 2013-2016 Memba Sarl. All rights reserved.
+ * Sources at https://github.com/Memba
  */
+
+/* jshint browser: true */
+/* globals define: false */
+
+(function (f, define) {
+    'use strict';
+    define([
+        '../vendor/kendo/cultures/kendo.culture.en-GB.js'
+        // '../vendor/kendo/messages/kendo.messages.en-US.js',
+        // '../messages/kidoju.messages.en.js'
+    ], f);
+})(function () {
+
+    'use strict';
+
+    (function () {
+        var app = window.app = window.app || {};
+        app.cultures = app.cultures || {};
+        app.cultures.en = {
+            // Drawer
+            drawer: {
+                explore: 'Explore',
+                favourites: 'Favourites',
+                scores: 'Scores',
+                settings: 'Settings'
+            },
+            // Categories view
+            // Summaries view
+            // Settings view
+            settings: {
+                user: 'User',
+                version: 'Version',
+                language: 'Language',
+                theme: 'Theme'
+            }
+        };
+        window.kendo.culture('en-GB');
+    }());
+
+    return window.app;
+
+}, typeof define === 'function' && define.amd ? define : function (_, f) { 'use strict'; f(); });
--- js/cultures/app.culture.fr.js
+++ js/cultures/app.culture.fr.js
@@ -1,3 +1,46 @@
 /**
- * Created by jlche on 23/03/2016.
+ * Copyright (c) 2013-2016 Memba Sarl. All rights reserved.
+ * Sources at https://github.com/Memba
  */
+
+/* jshint browser: true */
+/* globals define: false */
+
+(function (f, define) {
+    'use strict';
+    define([
+        '../vendor/kendo/cultures/kendo.culture.fr-FR.js',
+        '../vendor/kendo/messages/kendo.messages.fr-FR.js',
+        '../messages/kidoju.messages.fr.js'
+    ], f);
+})(function () {
+
+    'use strict';
+
+    (function () {
+        var app = window.app = window.app || {};
+        app.cultures = app.cultures || {};
+        app.cultures.fr = {
+            // Drawer
+            drawer: {
+                explore: 'Explorer',
+                favourites: 'Favoris',
+                scores: 'Scores',
+                settings: 'Configuration'
+            },
+            // Categories view
+            // Summaries view
+            // Settings view
+            settings: {
+                user: 'Utilisateur',
+                version: 'Version',
+                language: 'Langue',
+                theme: 'Thème'
+            }
+        };
+        window.kendo.culture('fr-FR');
+    }());
+
+    return window.app;
+
+}, typeof define === 'function' && define.amd ? define : function (_, f) { 'use strict'; f(); });
