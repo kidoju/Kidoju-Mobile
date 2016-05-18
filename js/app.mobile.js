85091fee1d8e8e24024abc550fc3e46a90d5103a
jlchereau <jlchereau@msn.com>
2016-05-18 11:11:09 +0100

    Initial app structure

--- js/app.mobile.js
+++ js/app.mobile.js
@@ -21,12 +21,31 @@ if (typeof(require) === 'function') {
 (function (f, define) {
     'use strict';
     define([
-        './vendor/kendo/kendo.all'
-        // './window.assert',
-        // './window.logger',
-        // './app.logger',
+        './vendor/kendo/kendo.core',
+        './vendor/kendo/kendo.fx',
+        './vendor/kendo/kendo.userevents',
+        './vendor/kendo/kendo.draganddrop',
+        './vendor/kendo/kendo.mobile.scroller',
+        './vendor/kendo/kendo.data',
+        './vendor/kendo/kendo.binder',
+        './vendor/kendo/kendo.view',
+        './vendor/kendo/kendo.mobile.view',
+        './vendor/kendo/kendo.mobile.loader',
+        './vendor/kendo/kendo.mobile.pane',
+        './vendor/kendo/kendo.router',
+        './vendor/kendo/kendo.mobile.application',
+        './vendor/kendo/kendo.mobile.drawer',
+        './vendor/kendo/kendo.mobile.button',
+        './vendor/kendo/kendo.mobile.listview',
+        './vendor/kendo/kendo.mobile.navbar',
+        './vendor/kendo/kendo.mobile.switch',
+        './vendor/kendo/kendo.touch',
+        './window.assert',
+        './window.logger',
+        './app.logger',
         // './app.i18n',
-        // './app.rapi'
+        './app.rapi',
+        './app.cache'
     ], f);
 })(function () {
 
@@ -35,47 +54,196 @@ if (typeof(require) === 'function') {
     (function ($, undefined) {
 
         var kendo = window.kendo;
-        var app = window.app || {};
+        var app = window.app = window.app || {};
+        var mobile = app.mobile = app.mobile || {};
+        var UNDEFINED = 'undefined';
 
-        // Application Constructor
-        app.initialize = function() {
-            this.bindEvents();
+        /*******************************************************************************************
+         * viewModel
+         *******************************************************************************************/
+
+        mobile.viewModel = kendo.observable({
+
+            /**
+             * Categories
+             */
+            categories: [
+                { name: 'Mathematics' },
+                { name: 'Physics' },
+                { name: 'English' }
+            ],
+            
+            /**
+             * Summaries
+             */
+            summaries: [
+                { title: 'Test of Mathematics' },
+                { title: 'Test of Physics' },
+                { title: 'Test of English' },
+                { title: 'Test of Geography' },
+                { title: 'Test of History' }
+            ],
+
+            /**
+             * Favourites
+             */
+            favourites: [
+                { title: 'Test of Mathematics' },
+                { title: 'Test of Physics' },
+                { title: 'Test of English' },
+                { title: 'Test of Geography' },
+                { title: 'Test of History' }
+            ],
+
+            /**
+             * Scores
+             */
+            scores: [
+                { title: 'Test of Mathematics', score: 10 },
+                { title: 'Test of Physics', score: 20 },
+                { title: 'Test of English', score: 30 },
+                { title: 'Test of Geography', score: 40 },
+                { title: 'Test of History', score: 50 }
+            ],
+
+            /**
+             * Languages
+             */
+            languages: [
+                { value: 'en', text: 'English' },
+                { value: 'fr', text: 'French' }
+            ],
+
+            /**
+             * Themes
+             */
+            themes: [
+                'flat',
+                'nova'
+            ],
+
+            /**
+             * User settings
+             */
+            settings: {
+                picture: '',
+                userName: 'Jacques L. Chereau',
+                version: 'v0.0.10',
+                language: 'en'
+            }
+
+
+        });
+
+        /*******************************************************************************************
+         * Event handler and utility methods
+         *******************************************************************************************/
+
+        /**
+         * Event Handler trigger when the device is ready (this is a cordova event)
+         * Loads the application
+         */
+        mobile.onDeviceReady = function() {
+            mobile.application = new kendo.mobile.Application($('#phone'), { platform: 'android-dark', skin: 'nova' });
+        };
+
+        mobile._setNavBar = function (e) {
+            var showDrawerButton = false;
+            var showBackButton = false;
+            var showSearchButton = false;
+            var showSyncButton = false;
+            var showSelectionButtons = false;
+            switch (e.view.id) {
+                case '/':
+                case '#phone-categories':
+                    showDrawerButton = true;
+                    showBackButton = true;  // this depends whether category has parent id or not
+                    showSearchButton = true;
+                    break;
+                case '#phone-summaries':
+                    showDrawerButton = true;
+                    showBackButton = true;
+                    showSearchButton = true;
+                    showSelectionButtons = true;
+                    break;
+                case '#phone-favourites':
+                    showDrawerButton = true;
+                    showSyncButton = true;
+                    break;
+                case '#phone-scores':
+                    showDrawerButton = true;
+                    showSyncButton = true;
+                    break;
+                case '#phone-settings':
+                    showDrawerButton = true;
+                    showSyncButton = true;
+                    break;
+                case '#phone-player':
+                    showDrawerButton = true;
+                    // Add playbar buttons + page numbers
+                    break;
+            }
+            $('#phone-main-layout-drawer').css({ display: showDrawerButton ? 'inline-block': 'none' });
+            $('#phone-main-layout-back').css({ display: showBackButton ? 'inline-block': 'none' });
+            $('#phone-main-layout-search').css({ display: showSearchButton ? 'inline-block': 'none' });
+            $('#phone-main-layout-sync').css({ display: showSyncButton ? 'inline-block': 'none' });
+            $('#phone-main-layout-selection').css({ display: showSelectionButtons ? 'table' : 'none' });
         };
 
         /**
-         * Bind Event Listeners
-         *
-         * Bind any events that are required on startup. Common events are:
-         * 'load', 'deviceready', 'offline', and 'online'.
+         * Event handler triggered when showing the Categories view
+         * @param e
          */
-        app.bindEvents = function() {
-            document.addEventListener('deviceready', this.onDeviceReady, false);
+        mobile.onCategoriesViewShow = function (e) {
+            mobile._setNavBar(e);
         };
 
         /**
-         * deviceready Event Handler
-         *
-         * The scope of 'this' is the event. In order to call the 'receivedEvent'
-         * function, we must explicitly call 'app.receivedEvent(...);'
+         * Event handler triggered when showing the Summaries view
+         * @param e
          */
-        app.onDeviceReady = function() {
-            app.receivedEvent('deviceready');
+        mobile.onSummariesViewShow = function (e) {
+            mobile._setNavBar(e);
         };
 
         /**
-         * Update DOM on a Received Event
-         * @param id
+         * Event handler triggered when showing the Favourites view
+         * @param e
          */
-        app.receivedEvent = function(id) {
-            var parentElement = document.getElementById(id);
-            var listeningElement = parentElement.querySelector('.listening');
-            var receivedElement = parentElement.querySelector('.received');
-
-            listeningElement.setAttribute('style', 'display:none;');
-            receivedElement.setAttribute('style', 'display:block;');
-
-            console.log('Received Event: ' + id);
+        mobile.onFavouritesViewShow = function (e) {
+            mobile._setNavBar(e);
         };
+
+        /**
+         * Event handler triggered when showing the Settings view
+         * @param e
+         */
+        mobile.onSettingsViewShow = function (e) {
+            mobile._setNavBar(e);
+        };
+
+        /**
+         * Event handler triggered when showing the Player view
+         * @param e
+         */
+        mobile.onPlayerViewShow = function (e) {
+            mobile._setNavBar(e);
+        };
+
+        /*******************************************************************************************
+         * Application initialization
+         *******************************************************************************************/
+
+        $(document).ready(function() {
+            if ($.type(window.device) !== UNDEFINED && $.type(window.device.cordova) !== UNDEFINED) {
+                //Wait for Cordova to load
+                document.addEventListener('deviceready', mobile.onDeviceReady, false);
+            } else {
+                //no need to wait when running/debugging on a PC
+                mobile.onDeviceReady();
+            }
+        });
+
 
     }(window.jQuery));
 
