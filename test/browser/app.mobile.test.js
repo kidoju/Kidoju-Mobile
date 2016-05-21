30eca22082cb53e9d553f1f21e434f8a5f217f60
jlchereau <jlchereau@msn.com>
2016-05-21 08:47:37 +0100

    Added browser tests

---
+++ test/browser/app.mobile.test.js
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) 2013-2016 Memba Sarl. All rights reserved.
+ * Sources at https://github.com/Memba
+ */
+
+/* jshint browser: true */
+/* global describe, it, expect */
+
+;(function (window) {
+
+    'use strict';
+
+    var expect = window.chai.expect;
+
+    describe('Test PageItemCollectionDataSource', function () {
+
+        describe('When doing something', function () {
+            it('We expect something else', function () {
+                expect(true).to.equal(true);
+            });
+        });
+
+    });
+
+}(this));
