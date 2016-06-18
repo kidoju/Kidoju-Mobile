5f8630ed486d2970c5c2c6538d0b0b55dc2e0e3b
jlchereau <jlchereau@msn.com>
2016-06-18 08:47:29 +0100

    Added more plugins

--- plugins.cmd
+++ plugins.cmd
@@ -1,2 +1,4 @@
 phonegap plugin add cordova-plugin-dialogs
+phonegap plugin add cordova-plugin-inappbrowser
+phonegap plugin add cordova-plugin-splashscreen
 phonegap plugin add cordova-plugin-statusbar
