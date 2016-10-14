bfb960bf0c7a85e76a51d0947349bf369f91eb2c
jlchereau <jlchereau@msn.com>
2016-10-14 09:07:26 +0100

    Updated with most recent plugins

--- plugins.cmd
+++ plugins.cmd
@@ -1,4 +1,7 @@
 phonegap plugin add cordova-plugin-dialogs
 phonegap plugin add cordova-plugin-inappbrowser
+phonegap plugin add cordova-plugin-secure-storage
 phonegap plugin add cordova-plugin-splashscreen
 phonegap plugin add cordova-plugin-statusbar
+phonegap plugin add cordova-plugin-x-socialsharing
+phonegap prepare
