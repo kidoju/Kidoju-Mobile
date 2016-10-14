7a166a9f48fbb2f85e783381c4b8a1d8089a7c30
jlchereau <jlchereau@msn.com>
2016-10-14 16:02:37 +0100

    Added barcode scanner

--- js/app.mobile.js
+++ js/app.mobile.js
@@ -828,18 +828,16 @@ if (typeof(require) === 'function') {
                             alert("Scanning failed: " + error);
                         },
                         {
-                            "preferFrontCamera": true, // iOS and Android
+                            "preferFrontCamera": false, // iOS and Android
                             "showFlipCameraButton": true, // iOS and Android
                             "prompt": "Place a barcode inside the scan area", // supported on Android only
-                            "formats": "QR_CODE", // default: all but PDF_417 and RSS_EXPANDED
-                            "orientation": "landscape" // Android only (portrait|landscape), default unset so it rotates with the device
+                            "formats": "QR_CODE" // default: all but PDF_417 and RSS_EXPANDED
+                            // "orientation": "landscape" // Android only (portrait|landscape), default unset so it rotates with the device
                         }
                     );
                 } else {
                     window.navigator.notification.alert('no barcode scanner...', null, 'Error', 'OK');
                 }
-            } else {
-                window.navigator.notification.alert('Wrong item...', null, 'Error', 'OK');
             }
         };
 
