eeee09bc5e66cb7efecf049258cd5f07b8f839e9
jlchereau <jlchereau@msn.com>
2016-10-17 16:44:09 +0100

    Added cordova-plugin-customurlscheme

--- plugins.cmd
+++ plugins.cmd
@@ -1,10 +1,11 @@
-REM cordova plugin add cordova-plugin-customurlscheme --variable URL_SCHEME=mycoolapp
-phonegap plugin add cordova-plugin-dialogs
+phonegap plugin add cordova-plugin-customurlscheme --variable URL_SCHEME=kidoju --save
+phonegap plugin add cordova-plugin-dialogs --save
 REM cordova plugin add cordova-plugin-google-analytics
-phonegap plugin add cordova-plugin-inappbrowser
-phonegap plugin add cordova-plugin-secure-storage
-phonegap plugin add cordova-plugin-splashscreen
-phonegap plugin add cordova-plugin-statusbar
-phonegap plugin add cordova-plugin-x-socialsharing
-phonegap plugin add phonegap-plugin-barcodescanner --variable CAMERA_USAGE_DESCRIPTION="To scan barcodes"
+phonegap plugin add cordova-plugin-inappbrowser --save
+phonegap plugin add cordova-plugin-secure-storage --save
+phonegap plugin add cordova-plugin-splashscreen --save
+phonegap plugin add cordova-plugin-statusbar --save
+phonegap plugin add cordova-plugin-x-socialsharing --save
+phonegap plugin add phonegap-plugin-barcodescanner --variable CAMERA_USAGE_DESCRIPTION="To Scan QR Codes" --save
+
 phonegap prepare
