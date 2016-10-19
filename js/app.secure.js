115681e93c0559dbf6503179b0f2640f3c7e8064
jlchereau <jlchereau@msn.com>
2016-10-19 16:28:31 +0100

    New build

--- js/app.secure.js
+++ js/app.secure.js
@@ -39,39 +39,30 @@
             assert.isUndefined(this._ss, '`this._ss` should be undefined when calling init');
             var that = this;
             var alert = window.navigator.notification.alert;
-            alert('SecureStorage: ' + !!(window.cordova && window.cordova.plugins && window.cordova.plugins.SecureStorage));
             if (window.cordova && window.cordova.plugins && window.cordova.plugins.SecureStorage) {
                 that._ss = new window.cordova.plugins.SecureStorage(
                     function () {
-                        alert('SecureStorage initialized'); // TODO: remove
                         logger.debug({
                             message: 'SecureStorage successfully initialized',
                             method: 'SecureStorage.prototype.init'
                         })
                     },
-                    function (error) {
+                    function () {
                         // See: https://github.com/Crypho/cordova-plugin-secure-storage#users-must-have-a-secure-screen-lock-set
-                        alert(error);
-                        alert(
-                            'Please enable the screen lock on your device. This app cannot operate securely without it.',
+                        that._ss.secureDevice(
                             function () {
-                                that._ss.secureDevice(
-                                    function () {
-                                        alert('Screen lock enabled. Enjoy our secure features.');
-                                    },
-                                    function () {
-                                        alert(
-                                            'Screen lock disabled. Sorry, but our app cannot store user pins without it.',
-                                            function () {
-                                                window.navigator.app.exitApp();
-                                            },
-                                            'Exit'
-                                        );
-
-                                    }
-                                );
+                                alert('Screen lock enabled. Enjoy our secure features.', undefined, 'Information');
                             },
-                            'Screen Lock'
+                            function () {
+                                alert(
+                                    'Screen lock disabled. Sorry, but our app cannot store user pins without it.',
+                                    function () {
+                                        window.navigator.app.exitApp();
+                                    },
+                                    'Error'
+                                );
+
+                            }
                         );
                     },
                     name);
