4d0c2a84e6f08c1633db537c84f239859c29651f
jlchereau <jlchereau@msn.com>
2016-10-23 18:57:28 +0100

    Added cordova-plugin-google-analytics

--- plugins.cmd
+++ plugins.cmd
@@ -2,7 +2,7 @@ phonegap plugin add cordova-plugin-console --save
 phonegap plugin add cordova-plugin-crosswalk-webview --save
 phonegap plugin add cordova-plugin-customurlscheme --variable URL_SCHEME=kidoju --save
 phonegap plugin add cordova-plugin-dialogs --save
-REM cordova plugin add cordova-plugin-google-analytics
+phonegap plugin add cordova-plugin-google-analytics --save
 phonegap plugin add cordova-plugin-inappbrowser --save
 phonegap plugin add cordova-plugin-secure-storage --save
 phonegap plugin add cordova-plugin-splashscreen --save
