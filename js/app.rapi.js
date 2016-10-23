e17b3b2a581b8c2007766475345cd559daadbd81
jlchereau <jlchereau@msn.com>
2016-10-23 10:11:55 +0100

    Copied from Kidoju.Server

--- js/app.rapi.js
+++ js/app.rapi.js
@@ -276,7 +276,7 @@
             parseToken: function (url) {
                 // When running tests with grunt.mochaTest, the url is a file url - file:///C:/Users/Jacques-Louis/Creative Cloud Files/Kidoju/Kidoju.Server/test/client/app.cache.test.html
                 // Also this assert fails in Phonegap InAppBrowser
-                assert.match(RX_URL, url, rapi.util.format(assert.messages.match.default, 'url', RX_URL));
+                // assert.match(RX_URL, url, rapi.util.format(assert.messages.match.default, 'url', RX_URL));
                 var pos1 = rapi.util.getAccessTokenHashPos(url);
                 var qs = {};
                 var token = {};
