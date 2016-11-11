f6b7c182dd05b8c531cdbc725542681a70dc50c1
jlchereau <jlchereau@msn.com>
2016-11-11 08:20:33 +0000

    Added comment

--- plugins.sh
+++ plugins.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# There are dependencies between plugins and some plugins might not be removed like cordova-plugin-file which would make add fail
+# At some point the best way to circumvent that is to update version numbers in config.xml, then remove the platform and add the platform again
+
 PLUGINS=$(phonegap plugin list | awk '{print $1'})
 
 for PLUGIN in $PLUGINS; do
