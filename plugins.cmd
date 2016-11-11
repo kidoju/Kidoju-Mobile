bf1da790cc8148e753329a8d94dd59c4b41252e3
jlchereau <jlchereau@msn.com>
2016-11-11 08:06:29 +0000

    Added cordova-plugin-tts

--- plugins.cmd
+++ plugins.cmd
@@ -7,6 +7,7 @@ phonegap plugin add cordova-plugin-inappbrowser --save
 phonegap plugin add cordova-plugin-secure-storage --save
 phonegap plugin add cordova-plugin-splashscreen --save
 phonegap plugin add cordova-plugin-statusbar --save
+phonegap plugin add cordova-plugin-tts --save
 phonegap plugin add cordova-plugin-wkwebview-engine --save
 phonegap plugin add cordova-plugin-x-socialsharing --save
 phonegap plugin add phonegap-plugin-barcodescanner --variable CAMERA_USAGE_DESCRIPTION="To Scan QR Codes" --save
