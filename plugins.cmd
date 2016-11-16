b43a0ab58118b73dee534cce8b02384a3b1ed875
jlchereau <jlchereau@msn.com>
2016-11-16 11:44:06 +0000

    Removed cordova-plugin-secure-storage

--- plugins.cmd
+++ plugins.cmd
@@ -1,13 +1,18 @@
 phonegap plugin add cordova-plugin-console --save
 phonegap plugin add cordova-plugin-crosswalk-webview --save
 phonegap plugin add cordova-plugin-customurlscheme --variable URL_SCHEME=kidoju --save
+phonegap plugin add cordova-plugin-device --save
 phonegap plugin add cordova-plugin-dialogs --save
+phonegap plugin add cordova-plugin-file --save
+phonegap plugin add cordova-plugin-file-transfer --save
 phonegap plugin add cordova-plugin-google-analytics --save
 phonegap plugin add cordova-plugin-inappbrowser --save
-phonegap plugin add cordova-plugin-secure-storage --save
+phonegap plugin add cordova-plugin-network-information --save
+REM phonegap plugin add cordova-plugin-secure-storage --save
 phonegap plugin add cordova-plugin-splashscreen --save
 phonegap plugin add cordova-plugin-statusbar --save
 phonegap plugin add cordova-plugin-tts --save
+phonegap plugin add cordova-plugin-whitelist --save
 phonegap plugin add cordova-plugin-wkwebview-engine --save
 phonegap plugin add cordova-plugin-x-socialsharing --save
 phonegap plugin add phonegap-plugin-barcodescanner --variable CAMERA_USAGE_DESCRIPTION="To Scan QR Codes" --save
