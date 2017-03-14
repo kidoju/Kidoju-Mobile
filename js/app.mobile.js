1dd6227f9ca3cf0e2c87e0fb3e9dc08c925c510c
jlchereau <jlchereau@msn.com>
2017-03-14 09:15:36 +0000

    Center stage (especially on iPad in landscape mode) https://github.com/kidoju/Kidoju-Mobile/issues/101

--- js/app.mobile.js
+++ js/app.mobile.js
@@ -1863,25 +1863,53 @@ if (typeof(require) === 'function') {
                 var scale;
                 var infoHeight = 0;
                 var infoWidth = 0;
+                var proportion = 1;
                 if (width > height) { // landsacpe mode
                     // Note: we want the info panel to be between 25% and 33% of the screen real estate and at least 200px wide
                     infoWidth = Math.min(0.33 * width, Math.max(200, 0.25 * width, width - height * WIDTH  / HEIGHT));
-                    // So we need to recalculate the stage scale
-                    scale = Math.min((width - infoWidth) / WIDTH, height / HEIGHT);
+                    // now look at the proportion of what is left for our stage
+                    if ((width - infoWidth) / height > WIDTH / HEIGHT) {
+                        // There is room to set the stage full height and increase our info panel
+                        scale = height / HEIGHT;
+                        infoWidth = width - scale * WIDTH;
+                    } else {
+                        // There is no room to set the stage full height, so let's set a proportion to add a border
+                        scale = (width - infoWidth) / WIDTH;
+                        proportion = 0.95;
+                    }
                 } else { // portrait mode
                     // Note: we want the info panel to be between 25% and 33% of the screen real estate and at least 100px high
-                    // infoHeight = Math.min(0.33 * height, Math.max(180, 0.25 * height, height - width * HEIGHT / WIDTH));
-                    infoHeight = Math.max(100, 0.25 * height, height - width * HEIGHT / WIDTH);
-                    // So we need to recalculate the stage scale
-                    scale = Math.min((height - infoHeight) / HEIGHT, width / WIDTH);
+                    infoHeight = Math.min(0.33 * height, Math.max(180, 0.25 * height, height - width * HEIGHT / WIDTH));
+                    // now look at the proportion of what is left for our stage
+                    if ((height - infoHeight) / width > HEIGHT / WIDTH) {
+                        // There is room to set the stage full width and increase our info panel
+                        scale = width / WIDTH;
+                        infoHeight = height - scale * HEIGHT;
+                    } else {
+                        // There is no room to set the stage full width, so let's set a proportion to add a border
+                        scale = (height - infoHeight) / HEIGHT;
+                        proportion = 0.95;
+                    }
                 }
                 // Resize the stage
-                stageWidget.scale(scale);
+                stageWidget.scale(proportion * scale);
                 var stageWrapper = stageElement.parent();
                 assert.ok(stageWrapper.hasClass('kj-stage'), 'Stage wrapper is expected to have class `kj-stage`');
                 var stageContainer = stageWrapper.closest('.stretched-item');
-                stageContainer.height(Math.floor(scale * HEIGHT));
-                stageContainer.width(Math.floor(scale * WIDTH));
+                stageContainer
+                    .outerWidth(width - Math.ceil(infoWidth)) // We use Math.ceil instead of Math.floor here to get the leeway required by Microsoft Edge in embedded mode
+                    .outerHeight(height - Math.ceil(infoHeight))
+                    .css('position', 'relative');
+                stageContainer.children('.centered')
+                    .width(proportion * scale * WIDTH)
+                    .height(proportion * scale * HEIGHT)
+                    .css({
+                        position: 'absolute',
+                        top: '50%',
+                        left: '50%',
+                        marginLeft: -proportion * scale * WIDTH / 2,
+                        marginTop: -proportion * scale * HEIGHT / 2
+                    });
                 // Resize the markdown container and scroller for instructions/explanations
                 var markdownElement = contentElement.find(kendo.roleSelector('markdown'));
                 var markdownScrollerElement = markdownElement.closest(kendo.roleSelector('scroller'));
@@ -1889,8 +1917,15 @@ if (typeof(require) === 'function') {
                 assert.instanceof(kendo.mobile.ui.Scroller, markdownScroller, kendo.format(assert.messages.instanceof.default, 'markdownScroller', 'kendo.mobile.ui.Scroller'));
                 var markdownContainer = markdownElement.closest('.stretched-item');
                 var markdownHeading = markdownContainer.children('.heading');
-                markdownContainer.outerHeight((height > width) ? height - stageContainer.outerHeight() : height);
-                markdownContainer.outerWidth((height > width) ? width : width - stageContainer.outerWidth());
+                markdownContainer
+                    .outerWidth(Math.floor(infoWidth) || width)
+                    .outerHeight(Math.floor(infoHeight) || height)
+                    .css({
+                        borderTopWidth: infoHeight ? 1 : 0,
+                        borderRightWidth: 0,
+                        borderBottomWidth: 0,
+                        borderLeftWidth: infoWidth ? 1 : 0
+                    });
                 markdownScroller.destroy();
                 markdownScrollerElement.outerHeight(markdownContainer.height() - markdownHeading.outerHeight() - parseInt(markdownContainer.css('padding-bottom'), 10));
                 var markdownScrollerWidget = markdownScrollerElement.kendoMobileScroller().data('kendoMobileScroller');
