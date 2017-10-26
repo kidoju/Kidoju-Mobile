eb3407a8aa8f1dc29943935c0f29ee545d2c293c
jlchereau <jlchereau@msn.com>
2017-10-26 21:07:06 +0100

    Added getVoices

--- js/app.tts.js
+++ js/app.tts.js
@@ -19,13 +19,34 @@
 
     var app = window.app = window.app || {};
     var tts = app.tts = app.tts || {};
-    var UNDEFINED = 'undefined';
-    var CHUNK_SIZE = 175;
 
     (function ($, undefined) {
 
         var assert = window.assert;
         var logger = new window.Logger('app.tts');
+        var BOOLEAN = 'boolean';
+        var NUMBER = 'number';
+        var STRING = 'string';
+        var UNDEFINED = 'undefined';
+        var CHUNK_SIZE = 175;
+        var voices= [];
+        var loadVoices = function () {
+            voices = window.speechSynthesis.getVoices();
+        };
+
+        /* NOTES
+        On Android 5 (Nexus 7), I could not get the sound from W3C Speech APIs to work before changing some Android settings I could not reproduce
+        in Settings > System > Accessibility > Text-to-speech output as explained at https://www.greenbot.com/article/2105862/android/how-to-get-started-with-google-text-to-speech.html
+        */
+
+        /**
+         * Load voices
+         */
+        if ('speechSynthesis' in window) {
+            loadVoices();
+            // Chrome loads voices asynchronously, which means the previous might have returned an empty array
+            window.speechSynthesis.onvoiceschanged = loadVoices;
+        }
 
         /**
          * Test cordova plugin
@@ -34,16 +55,33 @@
          */
         tts._useCordovaPlugIn = function () {
             // This has to be a function because it needs to be evaluated once the TTS plugin is loaded
-            // return !!(window.cordova && window.device && window.device.platform !== 'browser' && window.TTS && $.isFunction(window.TTS.speak));
-            return false;
+            return !!(window.cordova && window.device && window.device.platform !== 'browser' && window.TTS && $.isFunction(window.TTS.speak));
         };
 
         /**
          * Test HTML5 Speech API
+         * @see https://codepen.io/matt-west/pen/wGzuJ
          */
         tts._useSpeechSynthesis = function () {
-            // return !!('speechSynthesis' in window && $.isFunction(window.speechSynthesis.speak) && $.isFunction(window.SpeechSynthesisUtterance));
-            return true;
+            return !!('speechSynthesis' in window && $.isFunction(window.speechSynthesis.speak) && $.isFunction(window.SpeechSynthesisUtterance));
+        };
+
+        /**
+         * Get voice
+         * @see https://developer.mozilla.org/en-US/docs/Web/API/SpeechSynthesisVoice
+         * Edge and Firefox only load the voices corresponding to browser language settings
+         * Chrome and Safari load all voice languages
+         * This can be tested at https://codepen.io/matt-west/pen/wGzuJ
+         */
+        tts._getVoice = function (language) {
+            assert.type(STRING, language, assert.format(assert.messages.type.default, 'language', STRING));
+            var natives = voices.filter(function (voice) { return voice.lang.toLowerCase().startsWith(language.toLowerCase()); });
+            var localDefaults = natives.filter(function (voice) { return voice.default && voice.localService; });
+            if (Array.isArray(localDefaults) && localDefaults.length) {
+                return localDefaults[0];
+            } else if (Array.isArray(natives) && natives.length) {
+                return natives[0];
+            }
         };
 
         /**
@@ -53,20 +91,24 @@
          * @private
          */
         tts._clearMarkdown = function (markdown) {
-            return (markdown || '')
+            assert.type(STRING, markdown, assert.format(assert.messages.type.default, 'markdown', STRING));
+            return markdown
                 .replace(/[#`>_\*]/g, '') // remove headings, code (backticks), emphasis
                 .replace(/!?\[([^\]]+)\]\([^\)]+\)/g, '$1'); // remove web and image links
         };
 
         /**
-         * Chrome does not speak text longer than approx. 300 characters,
+         * Chrome version < 56 does not speak text longer than approx. 300 characters,
          * so we need to chunk text into an array of smaller strings to play
          * @see http://stackoverflow.com/questions/21947730/chrome-speech-synthesis-with-longer-texts
          * @see https://github.com/unk1911/speech/blob/master/js/speech.js
          * @param text
+         * @param size
          * @private
          */
         tts._chunk = function (text, size) {
+            assert.type(STRING, text, assert.format(assert.messages.type.default, 'text', STRING));
+            assert.type(NUMBER, size, assert.format(assert.messages.type.default, 'size', NUMBER));
             var ret = [];
             // Chromium is chrome or opera and window.StyleMedia excludes MS Edge
             // @see http://stackoverflow.com/questions/9847580/how-to-detect-safari-chrome-ie-firefox-and-opera-browser
@@ -102,30 +144,39 @@
          * @private
          */
         tts._speechSynthesisPromise = function (text, language) {
+            assert.type(STRING, text, assert.format(assert.messages.type.default, 'text', STRING));
+            assert.type(STRING, language, assert.format(assert.messages.type.default, 'language', STRING));
             var dfd = $.Deferred();
             if (tts._useSpeechSynthesis()) {
-                var utterance = new window.SpeechSynthesisUtterance(text);
-                if ($.type(window.StyleMedia) === UNDEFINED) {
-                    // Setting an unavailable language in Microsoft Edge breaks the speech
-                    // and we could not yet find how to list available languages
-                    utterance.lang = language;
-                }
-                // http://www.hongkiat.com/blog/text-to-speech/
-                // The first voice plays gibberish on iOS 10 and iOS 11, so let iOS use the default voice
-                // utterance.voice = window.speechSynthesis.getVoices()[0];
-                utterance.rate = 1;
-                utterance.onend = function (evt) { // Returns a SpeechSynthesisEvent
-                    if (evt.type === 'error') {
-                        // This occurs on Edge when the language pack has not been installed
-                        dfd.reject(new Error('Speech synthesis error'));
-                    } else {
-                        dfd.resolve(evt);
+                var voice = tts._getVoice(language);
+                if (voice && voice.lang) {
+                    var utterance = new window.SpeechSynthesisUtterance(text);
+                    // utterance.lang = language; // Setting an unavailable language in Microsoft Edge breaks the speech
+                    utterance.voice = voice;
+                    utterance.rate = 1;
+                    utterance.onend = function(evt) { // Returns a SpeechSynthesisEvent
+                        if (evt.type === 'error') {
+                            // This occurs on Edge when the language pack has not been installed
+                            dfd.reject(new Error('Speech synthesis error.'));
+                            logger.error({
+                                method: 'tts._speechSynthesisPromise',
+                                message: 'Speech synthesis error..'
+                            });
+                        } else {
+                            dfd.resolve(evt);
+                        }
+                    };
+                    utterance.onerror = dfd.reject;
+                    window.speechSynthesis.speak(utterance);
+                    if ('console' in window) {
+                        window.console.log(utterance); // a funny workaround to ensure the onend callback is called in Chrome
                     }
-                };
-                utterance.onerror = dfd.reject;
-                window.speechSynthesis.speak(utterance);
-                if ('console' in window) {
-                    window.console.log(utterance); // a funny workaround to ensure the onend callback is called in Chrome
+                } else {
+                    dfd.reject(new Error('No voice available to speak text.'));
+                    logger.error({
+                        method: 'tts._speechSynthesisPromise',
+                        message: 'No voice available to speak text.'
+                    });
                 }
             } else {
                 dfd.resolve();
@@ -140,6 +191,9 @@
          * @param clear
          */
         tts.doSpeak = function (text, language, clear) {
+            assert.type(STRING, text, assert.format(assert.messages.type.default, 'text', STRING));
+            assert.type(STRING, language, assert.format(assert.messages.type.default, 'language', STRING));
+            assert.type(BOOLEAN, clear, assert.format(assert.messages.type.default, 'clear', BOOLEAN));
             var dfd = $.Deferred();
             if (clear) {
                 text = tts._clearMarkdown(text);
@@ -148,6 +202,10 @@
                 // For iOS and Android via TTS plugin
                 // Note: iOS WKWebView engine for cordova supports speechSynthesis (see other branch of if) but does not output any sound
                 window.TTS.speak({ text: text, locale: language === 'fr' ? 'fr-FR' : 'en-US', rate: 1.5 }, dfd.resolve, dfd.reject);
+                logger.debug({
+                    method: 'tts.doSpeak',
+                    message: 'Text spoken with Cordova Plug-in'
+                });
             } else if (tts._useSpeechSynthesis()) {
                 // In the browser - https://developer.mozilla.org/en-US/docs/Web/API/SpeechSynthesisUtterance
                 var chunks = tts._chunk(text, CHUNK_SIZE);
@@ -158,6 +216,10 @@
                 $.when.apply(null, promises)
                     .done(dfd.resolve)
                     .fail(dfd.reject);
+                logger.debug({
+                    method: 'tts.doSpeak',
+                    message: 'Text spoken with W3C Speech API'
+                });
             } else {
                 dfd.resolve();
             }
@@ -173,12 +235,18 @@
                 // For iOS and Android via TTS plugin
                 // @see http://ourcodeworld.com/articles/read/370/how-to-convert-text-to-speech-speech-synthesis-in-cordova
                 window.TTS.speak('', dfd.resolve, dfd.reject);
+                logger.debug({
+                    method: 'tts.cancelSpeak',
+                    message: 'Text canceled with Cordova Plug-in'
+                });
             }  else if (tts._useSpeechSynthesis()) {
                 // In the browser - https://developer.mozilla.org/en-US/docs/Web/API/SpeechSynthesis/cancel
                 window.speechSynthesis.cancel();
                 dfd.resolve();
-            } else {
-                dfd.resolve();
+                logger.debug({
+                    method: 'tts.cancelSpeak',
+                    message: 'Text canceled with W3C Speech API'
+                });
             }
             return dfd.promise();
         };
