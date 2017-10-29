801d6654b9a7a6ad4c31a60cfd155a5fb33b10c9
jlchereau <jlchereau@msn.com>
2017-10-29 09:02:20 +0000

    Filter by organization, author, category, ... for white label version of app https://github.com/kidoju/Kidoju-Mobile/issues/75

---
+++ js/app.constants.js
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) 2013-2017 Memba Sarl. All rights reserved.
+ * Sources at https://github.com/Memba
+ */
+
+/* jshint browser: true, jquery: true */
+/* globals define: false, require: false */
+
+(function (f, define) {
+    'use strict';
+    define([], f);
+})(function () {
+
+    'use strict';
+
+    var app = window.app = window.app || {};
+
+    (function () {
+
+        // These constants lock the app to certain values
+
+        app.constants = {
+            // The authorId to search summaries from
+            authorId: '',
+            // The root categoryId
+            categoryId: '',
+            // The app language
+            language: 'fr',
+            // The app theme
+            theme: ''
+        }
+
+    }());
+
+    return app;
+
+}, typeof define === 'function' && define.amd ? define : function (_, f) { 'use strict'; f(); });
