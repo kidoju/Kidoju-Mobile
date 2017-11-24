e982b3e6ddef9e980537711c00d4cdc54b7a60a6
jlchereau <jlchereau@msn.com>
2017-11-24 19:51:20 +0000

    New database upgrade framework

---
+++ test/browser/app.db.upgrade.test.js
@@ -0,0 +1,89 @@
+/**
+ * Copyright (c) 2013-2017 Memba Sarl. All rights reserved.
+ * Sources at https://github.com/Memba
+ */
+
+/* jshint browser: true, expr: true */
+/* global describe, it, before */
+
+;(function ($, undefined) {
+
+    'use strict';
+
+    var expect = window.chai.expect;
+    var kendo = window.kendo;
+    var pongodb = window.pongodb;
+    var UNDEFINED = 'undefined';
+    var Database = kendo.Class.extend({
+        init: function (options) {
+            this._version = options.version;
+        },
+        version: function (value) {
+            if ($.type(value) === UNDEFINED) {
+                return $.Deferred().resolve(this._version).promise();
+            } else {
+                this._version = value;
+                return $.Deferred().resolve().promise();
+            }
+        }
+    });
+    function script() {
+        var that = this;
+        var dfd = $.Deferred();
+        var count = 0;
+        var interval = setInterval(function () {
+            count ++;
+            dfd.notify({ version: that._version, pass: 1, percent: count / 10 });
+            if (count === 10) {
+                clearInterval(interval);
+                dfd.resolve();
+            }
+        }, 10);
+        return dfd.promise();
+    }
+
+    describe('Upgrade', function () {
+
+        var db;
+        var spy = sinon.spy();
+
+        beforeEach(function () {
+            db = new Database({ version: '0.1.0' });
+        });
+
+        it('it should upgrade', function (done) {
+
+            var upgrade = new pongodb.Upgrade({ db: db });
+            upgrade.push(new pongodb.Migration({
+                version: '0.1.0',
+                scripts: [ script ]
+            }));
+            upgrade.push(new pongodb.Migration({
+                version: '0.1.1',
+                scripts: [ script ]
+            }));
+            upgrade.push(new pongodb.Migration({
+                version: '0.1.2',
+                scripts: [ script ]
+            }));
+            upgrade.execute()
+                .progress(function (state) {
+                    spy();
+                })
+                .always(function () {
+                    db.version()
+                        .done(function (version) {
+                            expect(spy).to.have.callCount(20);
+                            expect(version).to.equal('0.1.2');
+                        })
+                        .always(function () {
+                            done();
+                        });
+                });
+
+        });
+
+    });
+
+
+}(window.jQuery));
