6bf571b61c324a7e6b67ec6a61f76177174ecd70
jlchereau <jlchereau@msn.com>
2018-01-15 21:05:12 +0000

    Restored local notifications every 7 days

--- js/app.mobile.js
+++ js/app.mobile.js
@@ -3943,8 +3943,8 @@ window.jQuery.holdReady(true);
                         // firstAt: firstAt
                         // With version 0.9 - https://github.com/katzer/cordova-plugin-local-notifications
                         // trigger: { every: { weekday: 1, hour: 16, minute: 0 } },
-                        trigger: { every: 'hour', count: 1 },
-                        // trigger: { every: 'day', count: 7 },
+                        // trigger: { every: 'hour', count: 1 },
+                        trigger: { every: 'day', count: 7 },
                         foreground: true
                     });
                 });
