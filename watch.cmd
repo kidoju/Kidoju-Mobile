efe9d7a9e530b3e589e3f36d55613c29d09a854e
jlchereau <jlchereau@msn.com>
2019-05-16 17:56:25 +0100

    Upgraded jQuery from v3.4.0 to v3.4.1

--- watch.cmd
+++ watch.cmd
@@ -1,6 +1,6 @@
 set NODE_ENV=mobile
 REM without call this batch ends with grunt uglify
 call grunt uglify
-copy /Y .\js\vendor\jquery\jquery-3.4.0.min.js .\www\build\jquery.min.js
+copy /Y .\js\vendor\jquery\jquery-3.4.1.min.js .\www\build\jquery.min.js
 copy /Y .\webapp\public\build\workerlib.bundle.js .\www\build
 webpack --watch
