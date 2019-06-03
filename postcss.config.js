b7a54812fe110f57c186b6e3b83528922d5b2de7
jlchereau <jlchereau@msn.com>
2019-06-03 20:44:04 +0100

    Added cssnano compression

--- postcss.config.js
+++ postcss.config.js
@@ -5,12 +5,25 @@
 
 /* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
 
+'use strict';
+
 // const smartImport = require('postcss-smart-import')({ /* ...options */ }),
 // const precss = require('precss')({ /* ...options */ }),
 const autoprefixer = require('autoprefixer')({
     /* ...options */
 });
 
+const cssnano = require('cssnano')({
+    preset: [
+        'default',
+        {
+            discardComments: {
+                removeAll: true
+            }
+        }
+    ]
+});
+
 module.exports = {
-    plugins: [autoprefixer]
+    plugins: [autoprefixer, cssnano]
 };
